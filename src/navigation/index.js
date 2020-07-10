c6350e7215a9e4b16824c6210f3381dd82a8202f
Majid Lotfinia <majidlotfinia12@gmail.com>
2020-07-10 22:07:48 +0430

    add styles to bottom bar

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -5,12 +5,20 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './../screens/Home';
 import SettingScreen from './../screens/Setting';
 
+import {theme} from './../providers/themeProvider'
+
 const Tab = createBottomTabNavigator();
+
+const tabBarOptions = {
+  activeTintColor: theme.tabBarActiveColor,
+  inactiveTintColor: theme.primaryText,
+  style: {backgroundColor: theme.tabBarBackgroundColor, fontWeight: 'bold'},
+}
 
 export default function MainNavigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator tabBarOptions={tabBarOptions}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingScreen} />
       </Tab.Navigator>
