cb76cab5bd8eeb606811787d3d549da51f78d486
Majid Lotfinia <majidlotfinia12@gmail.com>
2020-07-10 21:35:40 +0430

    add some styles to screens

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,23 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 function HomeScreen() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to change React Native Theme Changer Sample APP</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
 
 export default HomeScreen;
--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 function SettingsScreen() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Settings!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+})
 
 export default SettingsScreen;
