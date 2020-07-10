7d9dcac4ef860282063b4aa17486674d8d20ff92
Majid Lotfinia <majidlotfinia12@gmail.com>
2020-07-10 23:03:59 +0430

    use asyn storage to save theme id for next app lunch

--- src/providers/themeProvider.js
+++ src/providers/themeProvider.js
@@ -1,14 +1,28 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import THEMES from './themes.json';
+
+const STORAGE_KEY = 'THEME_ID';
 
 const ThemeContext = React.createContext();
 
 export const ThemeContextProvider = ({ children }) => {
-    const [themeID, setThemeID] = useState('light');
+    const [themeID, setThemeID] = useState();
   
+    useEffect(() => {
+      (async () => {
+        const storedThemeID = await AsyncStorage.getItem(STORAGE_KEY);
+        if (storedThemeID) {
+          setThemeID(storedThemeID);
+        } else {
+          setThemeID('light');
+        }
+      })();
+    }, []);
+
     return (
       <ThemeContext.Provider value={{ themeID, setThemeID }}>
-        {children}
+        {!!themeID ? children : null}
       </ThemeContext.Provider>
     );
 };
@@ -17,9 +31,19 @@ export function withTheme(Component) {
     return props => {
       const { themeID, setThemeID } = useContext(ThemeContext);
   
+      function changeTheme (id) {
+        AsyncStorage.setItem(STORAGE_KEY, id);
+        setThemeID(id)
+      }
+
       const getTheme = themeID => THEMES[themeID];
-      const switchTheme = () => setThemeID(themeID === 'light' ? 'dark' : 'light');
-      const setTheme = (key) => setThemeID(key);
+      const switchTheme = () => {
+        const newTheme = themeID === 'light' ? 'dark' : 'light';
+        changeTheme(newTheme)
+      };
+      const setTheme = (key) => {
+        changeTheme(key)
+      };
   
       return (
         <Component
