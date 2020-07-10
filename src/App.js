a2c5eebcd11d6d8a3097a8bf0d7283581f8fdc93
Majid Lotfinia <majidlotfinia12@gmail.com>
2020-07-10 20:52:52 +0430

    change project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React form 'react';
+import {View, Text} from 'react-native';
+
+function App() {
+    return (
+        <View>
+            <Text>
+                RN theming
+            </Text>
+        </View>
+    )
+}
+
+export default App;
